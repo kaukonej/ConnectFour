c64c7a7a1b8ae4fbcdb2ade90fc7a7bcb0cbdc77
Justin <Justin@35.40.102.7>
2018-02-01 23:07:18 -0500

    Second Commit

---
+++ src/project2/ConnectFour.java
@@ -0,0 +1,38 @@
+package project2;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class ConnectFour {
+
+	//-----------------------------------------------------------------
+	//  Creates and displays the main program frame.
+	//-----------------------------------------------------------------
+	public static void main (String[] args) {
+	    JMenuBar menus;
+	    JMenu fileMenu;
+	    JMenuItem quitItem;
+	    JMenuItem gameItem;   
+
+		JFrame frame = new JFrame ("Connect Four");
+		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		
+        fileMenu = new JMenu("File");
+        quitItem = new JMenuItem("quit");
+        gameItem = new JMenuItem("new game");
+        
+        fileMenu.add(gameItem);
+        fileMenu.add(quitItem);
+        menus = new JMenuBar();
+        frame.setJMenuBar(menus);
+        menus.add(fileMenu);
+     
+        ConnectFourPanel panel = new ConnectFourPanel(quitItem,gameItem);
+		frame.getContentPane().add(panel);
+
+		frame.pack();
+		frame.setVisible(true);
+	} 
+}
---
+++ src/project2/ConnectFourPanel.java
@@ -0,0 +1,121 @@
+package project2;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class ConnectFourPanel extends JPanel implements ActionListener{
+	
+	private final int SIZE = 10;    
+	private JLabel[][] matrix;
+	private JButton[] selection;
+	
+	private JMenuItem newGameItem;
+	private JMenuItem quitItem;
+	private ImageIcon iconBlank;
+	private ImageIcon iconPlayer1;
+	private ImageIcon iconPlayer2;
+	
+	private ConnectFourGame game;
+	
+	public ConnectFourPanel(JMenuItem panelQuitItem, JMenuItem panelGameItem){
+		
+		game = new ConnectFourGame(SIZE);
+		
+		newGameItem = panelGameItem;
+		quitItem = panelQuitItem;
+		
+		iconBlank = new ImageIcon ("blank.png");
+		iconPlayer1 = new ImageIcon ("player1.png");
+		iconPlayer2 = new ImageIcon ("player2.png");
+		
+		quitItem.addActionListener(this);
+		newGameItem.addActionListener(this);       
+		
+		setLayout(new GridBagLayout());
+        GridBagConstraints loc = new GridBagConstraints();
+		
+		selection = new JButton[SIZE];
+		for (int col = 0; col < SIZE; col++) {
+			selection[col] = new JButton ("Select");
+			selection[col].addActionListener(this);
+			
+			// Add each element to the GridBagLayout
+	        loc = new GridBagConstraints();
+	        loc.gridx = col;
+	        loc.gridy = 0;
+	        loc.insets.bottom = 5;
+	        loc.insets.top = 5;
+	        add(selection[col], loc);
+		}
+		
+		matrix = new JLabel[SIZE][SIZE];
+		
+		for (int row = 0; row < SIZE; row++) {
+			for (int col = 0; col < SIZE; col++) {
+				matrix[row][col] = new JLabel("",iconBlank,SwingConstants.CENTER);
+				
+				// Add each element to the GridBagLayout
+		        loc = new GridBagConstraints();
+		        loc.gridx = col;
+		        loc.gridy = row + 1;
+		        loc.insets.bottom = 15;
+		        loc.insets.top = 15;
+		        add(matrix[row][col], loc);				
+			}
+		}
+	}
+	
+	private void clearBoard() {
+		game.reset();
+		for (int row = 0; row < SIZE; row++) 
+			for (int col = 0; col < SIZE; col++) 
+				matrix[row][col].setIcon(iconBlank);
+	}
+	
+	//--------------------------------------------------------------
+	//  Updates the counter and label when the button is pushed.
+	//--------------------------------------------------------------
+	public void actionPerformed (ActionEvent event)
+	{
+		
+		JComponent comp = (JComponent) event.getSource();
+		
+		for (int col = 0; col < SIZE; col++) {
+			if (selection[col] == comp) {
+				
+				int row = game.selectCol(col);
+				if (row == -1)
+					JOptionPane.showMessageDialog(null, "Col is full!");
+				else if (game.getTurn() == 0) {
+					//matrix[row][col].setIcon((game.getTurn() == 1) ? iconPlayer1 : iconPlayer2);
+					matrix[row][col].setIcon(iconPlayer1);
+					if (game.isWinner(0)) {
+						JOptionPane.showMessageDialog(null, "Player " + (game.getTurn() + 1) + " Wins!");
+						clearBoard();
+					}
+					game.finishTurn();
+				} else {
+					matrix[row][col].setIcon(iconPlayer2);
+					if (game.isWinner(1)) {
+						JOptionPane.showMessageDialog(null, "Player " + (game.getTurn() + 1) + " Wins!");
+						clearBoard();
+					}
+					game.finishTurn();
+				}
+				
+//				if (row != -1) {
+//					game.computerTurn();
+//				}
+			}
+		}
+		 
+		if (comp == newGameItem) {    
+			clearBoard();
+		}
+		
+		if (comp == quitItem)
+			System.exit(1);
+	}
+}
